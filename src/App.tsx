61ed92513c34668a537fa89f814cac67e0f4e632
justynalodzinska <justyna.lodzinska@smartive.app>
2024-05-15 21:53:27 +0200

    simple mqtt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mqtt from "mqtt";
 
-function App() {
+const App = () => {
+  const connectToMqtt = () => {
+    const client = mqtt.connect("mqtt://10.0.0.101");
+    console.log(client)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={connectToMqtt}>Connect</button>
+      <button>Next</button>
+      <button>Next</button>
+      <button>Next</button>
     </div>
   );
-}
+};
 
 export default App;
